ed51d0a81ad4955d6ad689928801b13ff6d94c5d
Ryan Karpuszka <rykarp5@gmail.com>
2021-10-30 13:19:31 -0400

    Fix bug on linux

--- run_gui.py
+++ run_gui.py
@@ -4,6 +4,10 @@ from PyQt5.QtWidgets import QApplication
 from gui.app import App
 from util import data_path
 
+import os
+
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+
 app = QApplication(sys.argv)
 a = App([os.path.join(data_path, 'example-streams', '1.mp4'), os.path.join(data_path, 'example-streams', '2.mp4')])
 a.show()
