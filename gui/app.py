5e1e34022d0426f706a96d9eef6ed644d32065b0
Noah Mollerstuen <noah@mollerstuen.com>
2021-11-07 13:50:22 -0500

    Refactor tabs system

--- gui/app.py
+++ gui/app.py
@@ -1,15 +1,25 @@
+import dataclasses
+import numpy as np
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QTabWidget
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QTabWidget
 from PyQt5.QtGui import QPixmap
 import cv2
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
-import numpy as np
 
 
-class Frame():
-    def __init__(self, cv_img, cam_index):
-        self.cv_img = cv_img
-        self.cam_index = cam_index
+@dataclasses.dataclass
+class Frame:
+    cv_img: np.ndarray
+    cam_index: int
+
+
+def convert_cv_qt(cv_img):
+    """Convert from an opencv image to QPixmap"""
+    rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    h, w, ch = rgb_image.shape
+    bytes_per_line = ch * w
+    convert_to_qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+    return QPixmap.fromImage(convert_to_qt_format)
 
 
 class VideoThread(QThread):
@@ -44,28 +54,72 @@ class VideoThread(QThread):
         self.wait()
 
 
+class RootTab(QWidget):
+    """An individual tab in the RootTabContainer containing all the widgets to be displayed in the tab"""
+
+    def __init__(self):
+        super().__init__()
+
+        # Create a new vbox layout to contain the tab's widgets
+        self.root_layout = QVBoxLayout(self)
+        self.setLayout(self.root_layout)
+
+
+class VideoTab(RootTab):
+    """A RootTab which displays video(s) from a camera stream or video file, among other functions"""
+
+    def __init__(self):
+        super().__init__()
+
+        # TODO: Replace with an instance of VideoArea and several VideoWidgets
+        # Create the labels that hold the images
+        self.image_labels = []
+        for i in range(0, 2):
+            self.image_labels.append(QLabel(self))
+
+        # Add the image labels to the tab
+        for label in self.image_labels:
+            self.root_layout.addWidget(label)
+
+    def handle_frame(self, frame: Frame):
+        qt_img = convert_cv_qt(frame.cv_img)
+
+        # Scale image
+        # TODO: VideoWidget should handle the scaling
+        scaled_img = qt_img.scaled(640, 480, Qt.KeepAspectRatio)
+
+        # Update the image label corresponding to the cam_index with the new frame
+        # TODO: Delegate frame to the tab's VideoArea, which should update all its VideoWidgets with the same cam_index
+        self.image_labels[frame.cam_index].setPixmap(scaled_img)
+
+
+class MainTab(VideoTab):
+    def __init__(self):
+        super().__init__()
+        # Add widgets specific to the "Main" tab here
+
+
+class DebugTab(VideoTab):
+    def __init__(self):
+        super().__init__()
+        # Add widgets specific to the "Debug" tab here
+
+
 class App(QWidget):
     def __init__(self, filenames):
         super().__init__()
         self.setWindowTitle("ROV Vision")
-        self.disply_width = 640
-        self.display_height = 480
+        self.resize(1280, 720)
+        self.showMaximized()
 
         # Create a tab widget
         self.tabs = QTabWidget()
-        self.main_tab = QWidget()
-        self.debug_tab = QWidget()
-        self.tabs.resize(300, 200)
+        self.main_tab = MainTab()
+        self.debug_tab = DebugTab()
 
+        self.tabs.resize(300, 200)
         self.tabs.addTab(self.main_tab, "Main")
         self.tabs.addTab(self.debug_tab, "Debug")
-
-        # Create a new vbox layout for each tab
-        self.main_layout = QVBoxLayout(self)
-        self.debug_layout = QVBoxLayout(self)
-
-        self.main_tab.setLayout(self.main_layout)
-        self.debug_tab.setLayout(self.debug_layout)
 
         # Create a vbox to hold the tabs widget
         vbox = QVBoxLayout()
@@ -73,16 +127,6 @@ class App(QWidget):
 
         # Set the root layout to this vbox
         self.setLayout(vbox)
-
-        # Create the labels that hold the images
-        self.image_labels = []
-        for i in range(0, 2):
-            self.image_labels.append(QLabel(self))
-            self.image_labels[i].resize(self.disply_width, self.display_height)
-
-        # Add the image labels to the main tab
-        for label in self.image_labels:
-            self.main_layout.addWidget(label)
 
         # Create the video capture thread
         self.thread = VideoThread(filenames)
@@ -97,16 +141,9 @@ class App(QWidget):
 
     @pyqtSlot(Frame)
     def update_image(self, frame: Frame):
-        """Updates the appropriate image_label with a new opencv image"""
-        qt_img = self.convert_cv_qt(frame.cv_img)
-        # to be replace with a loop thru VideoWidgets & comparison of frame's cam_index to videowidget's cam_index
-        self.image_labels[frame.cam_index].setPixmap(qt_img)
+        """Updates the appropriate tab with a new opencv image"""
 
-    def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
-        return QPixmap.fromImage(p)
+        # Update the tab which is currently being viewed only if it is a VideoTab
+        current_tab = self.tabs.currentWidget()
+        if isinstance(current_tab, VideoTab):
+            current_tab.handle_frame(frame)
