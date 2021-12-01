d9664d06d2c201bd3a7844c3d3f494f18d25fe7e
afazir <afaz99@gmail.com>
2021-11-30 20:39:57 -0500

    Made theme available without command terminal

--- gui/theme.py
+++ gui/theme.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import *
 def theme_picker(app, theme):
     if theme == 'dark_theme':
         dark_theme(app)
-    elif theme == 'alt_theme':
+    else:
         alt_theme(app)
 
 #sets dark theme
@@ -41,15 +41,15 @@ def alt_theme(app):
     # Now use a palette to switch to dark colors:
     palette = QPalette()
     palette.setColor(QPalette.Window, QColor(65, 241, 26))
-    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.WindowText, Qt.black)
     palette.setColor(QPalette.Base, QColor(25, 25, 25))
     palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
     palette.setColor(QPalette.ToolTipBase, Qt.white)
     palette.setColor(QPalette.ToolTipText, Qt.white)
     palette.setColor(QPalette.Text, Qt.white)
 
-    palette.setColor(QPalette.Button, QColor(254, 61, 250))
-    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.Button, QColor(255, 0 ,0))
+    palette.setColor(QPalette.ButtonText, Qt.black)
     palette.setColor(QPalette.BrightText, Qt.red)
     palette.setColor(QPalette.Link, QColor(42, 130, 218))
     palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
