4aff3c4388c84620f602ec493281843427226257
benjaminwp18 <bwp18@case.edu>
2022-02-12 14:26:04 -0500

    Add red mask decorater to dropdown

--- gui/decorated_functions.py
+++ gui/decorated_functions.py
@@ -1,5 +1,6 @@
 import cv2
 from gui.decorator import Decorator
+import numpy as np
 
 dropdown = Decorator()
 
@@ -22,3 +23,19 @@ def convert_to_gray(image):
     :return: image converted to grayscale
     """
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+@dropdown(name="Mask for Red")
+def convert_to_gray(image):
+    """
+    Converts the given image to a bitmap that masks anything other than red
+    :param image: image to convert
+    :return: converted image
+    """
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([155,25,0])
+    upper = np.array([179,255,255])
+    mask = cv2.inRange(hsv, lower, upper)
+
+    result = cv2.cvtColor(cv2.bitwise_and(hsv, hsv, mask=mask), cv2.COLOR_HSV2BGR)
+
+    return result
