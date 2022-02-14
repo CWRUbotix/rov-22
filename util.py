61c1cf9185fcc15c27f35446e6faeeb7eeae7e93
Ryan Karpuszka <rykarp5@gmail.com>
2022-02-14 17:01:59 -0500

    Make a default config file and ignore resoure-paths.json

--- util.py
+++ util.py
@@ -9,17 +9,18 @@ from logger import root_logger
 
 logger = root_logger.getChild(__name__)
 
+#If your repository paths do not match the following defaults, copy the "config/resource-paths.json.default" file to "config/resource-paths-paths.json" and edit.
 try:
-    with open("config/resource-paths.json", 'r') as paths_file:
+    with open('config/resource-paths.json', 'r') as paths_file:
         paths = json.load(paths_file)
-        data_path = path.abspath(paths["data_path"])
-        ardupilot_path = path.abspath(paths["ardupilot_path"])
-        gazebo_path = path.abspath(paths["gazebo_path"])
+        data_path = path.abspath(paths['data_path'])
+        ardupilot_path = path.abspath(paths['ardupilot_path'])
+        gazebo_path = path.abspath(paths['gazebo_path'])
 except (FileNotFoundError, json.JSONDecodeError, KeyError):
-    logger.debug("config/resource-paths.json not found or is invalid, using default resource paths")
+    logger.debug('config/resource-paths.json not found or is invalid, using default resource paths')
     parent_dir = path.split(path.dirname(__file__))[0]
-    data_path = path.join(parent_dir, "data")
-    ardupilot_path = path.join(parent_dir, 'ardupilot', 'ArduSub')
+    data_path = path.join(parent_dir, 'data')
+    ardupilot_path = path.join(parent_dir, 'ardupilot')
     gazebo_path = path.join(parent_dir, 'gazebo_rov')
 ardusub_path = path.join(ardupilot_path, 'ArduSub')
 
