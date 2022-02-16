540b1724955788ece2f32110764d0251d047807e
Georgia Martinez <georgiacmartinez@gmail.com>
2022-02-15 20:15:06 -0500

    Created TransectImage class

---
+++ vision/transect/transect_image.py
@@ -0,0 +1,18 @@
+"""For storing the eight transect images"""
+
+class TransectImage():
+
+    def __init__(self, image, num):
+        self.image = image
+        self.num = num
+
+    def image(self):
+        """Returns image"""
+
+        return self.image
+
+    def num(self):
+        """Returns num"""
+        return self.num
+
+    
