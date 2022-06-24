d4d335067e99b05f8e5d5e978e91e31f5619e6c5
georgia-martinez <gcm49@case.edu>
2022-06-23 22:48:57 -0700

    Changed clear button to clear only the previous frame

--- gui/widgets/transect_widget.py
+++ gui/widgets/transect_widget.py
@@ -36,7 +36,7 @@ class CaptureThread(QThread):
 class TransectWidget(QWidget):
     stitched_image_signal = pyqtSignal(np.ndarray)
 
-    pictures = []
+    picture_paths = []
     image_num = 0
 
     def __init__(self, app):
@@ -53,8 +53,8 @@ class TransectWidget(QWidget):
         self.capture_button = QPushButton("Capture Transect", self)
         self.capture_button.clicked.connect(self.capture)
 
-        self.clear_button = QPushButton("Clear Images", self)
-        self.clear_button.clicked.connect(self.clear_directory)
+        self.clear_button = QPushButton("Clear Previous", self)
+        self.clear_button.clicked.connect(self.clear_previous)
 
         self.manual_button = QPushButton("Stitch Manually", self)
         self.manual_button.clicked.connect(self.stitch_manually)     
@@ -75,13 +75,25 @@ class TransectWidget(QWidget):
 
         logger.info("Cleared the transect_frames folder in the data repo")
 
+    def clear_previous(self):
+        if self.image_num != 0:
+            os.remove(os.path.join(data_path, "transect_frames", self.picture_paths.pop()))
+            self.image_num -= 1
+            logger.info(f"Deleting transect frame {self.image_num+1}")
+        else:
+            logger.info("WARNING: There is no previous transect frame to delete")
+
     def capture(self):
         frame = self.app.get_active_frame().copy()
 
-        self.pictures.append(frame)
         self.image_num += 1
 
         self.capture_thread.set_image(frame, self.image_num)
+
+        file_name = f"transect_image({self.image_num}).png"
+        path = os.path.join(data_path, "transect_frames", file_name)
+
+        self.picture_paths.append(path)
 
         if not self.capture_thread.isRunning():
             self.capture_thread.start()
@@ -105,4 +117,4 @@ class TransectWidget(QWidget):
 
             self.transect_stitcher.initUI()
         else:
-            logger.info(f"WARNING: Need 8 photos, only {len(all_images)} were taken")
+            logger.info(f"WARNING: Need 8 photos, {len(all_images)} were taken")
